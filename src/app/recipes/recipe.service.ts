9a495287c958db922fe6f7692f81d090be57e5c7
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-08-26 00:03:45 +0300

    Creating the form for editing recipes. Delete recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,11 @@
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Ingredient } from './../shared/ingredient.model';
 import { Injectable } from '@angular/core';
-
 import {Recipe} from './recipe.model';
+import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService{
+    recipesChanged = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe1', 'This is simply a test1',
          'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg',
@@ -30,5 +31,20 @@ export class RecipeService{
 
       addIngredientToShoppingList(ingredients: Ingredient[]){
         this.slService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
       }
 }
